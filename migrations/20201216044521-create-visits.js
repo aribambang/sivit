303b20422c3e38a2f76d930ef2e7424700f2ffad
YuriWardana <tunmitunmi@gmail.com>
2020-12-16 11:58:44 +0700

    adding models

---
+++ migrations/20201216044521-create-visits.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('visits', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      VisitorId: {
+        type: Sequelize.INTEGER
+      },
+      CompanyId: {
+        type: Sequelize.INTEGER
+      },
+      ReceptionistId: {
+        type: Sequelize.INTEGER
+      },
+      start: {
+        type: Sequelize.DATE
+      },
+      end: {
+        type: Sequelize.DATE
+      },
+      purpose: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('visits');
+  }
+};
